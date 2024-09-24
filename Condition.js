6e3080df766e99da74d10c1661cb844908e7d5a9
Faruk'S Mac <faruksmac@FarukSs-Air.netis.cc>
2024-09-24 19:43:08 +0600

    first commit

---
+++ Condition.js
@@ -0,0 +1,11 @@
+var point = 15;
+var pointValue  = 30;
+
+if( point == 10 || pointValue == 20){
+    console.log("you are pass")
+}else if(point != 10 && pointValue == 15){
+    console.log("i am else if")
+}
+else{
+    console.log("you are fails")
+}
